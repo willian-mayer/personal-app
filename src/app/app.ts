4949d87152db3c8822283552e646ee01a3dbfd89
willian-mayer <willian.mayer.br@gmail.com>
2025-09-28 21:46:47 -0300

    adding navbar and deleting body margin

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Hero } from './components/hero/hero';
+import { Navbar } from './components/navbar/navbar';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Hero],
+  imports: [RouterOutlet, Hero, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css'
+})
+export class Navbar {
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
+}
