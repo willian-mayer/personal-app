2d0293f3270173cf7c300bd81a32d7c6ca798304
willian-mayer <willian.mayer.br@gmail.com>
2025-09-28 21:36:26 -0300

    adding hero-section

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Hero } from './components/hero/hero';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Hero],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
